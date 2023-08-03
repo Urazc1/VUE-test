661488ea369cb55bf1680d286229168ddfbb2335
dusT <997643500@qq.com>
2023-08-03 14:54:55 +0800

    delete trash

--- src/server/db.js
+++
@@ -1,9 +0,0 @@
-module.exports{
-    mysql: {
-        host: 'txs.dustcolor.top',
-        user: 'root',
-        password: '8b4cccacfcc48b0f',
-        database: 'testdb',
-        port: '3306'
-    }
-}
